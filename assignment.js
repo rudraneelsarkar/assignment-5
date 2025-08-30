6dc1a5038d6d358e0c4d6a44bd09288519f77830
rudraneelsarkar <nsa791677@gmail.com>
2025-08-31 04:35:54 +0900

    Added all files

---
+++ assignment.js
@@ -0,0 +1,81 @@
+    
+    // console.log('Js File Connected')
+
+    // Copy Button and Reaction Functionality 
+  const copyCounter = document.getElementById('copyCounter')
+  let count = 0;
+      
+          // function to design alert notification 
+   function showToast() {
+  const toast = document.getElementById("toast");
+  toast.classList.remove("hidden");
+  setTimeout(() => {
+    toast.classList.add("hidden");
+  }, 2000);
+}
+
+
+            //  copyBtn1 
+   document.getElementById('copyBtn1')
+   .addEventListener('click', function(){
+       showToast();
+       count ++;
+       copyCounter.textContent = count;
+   })
+            //  copyBtn2 
+   document.getElementById('copyBtn2')
+   .addEventListener('click', function(){
+       showToast();
+       count ++;
+       copyCounter.textContent = count;
+   })
+            //  copyBtn3 
+   document.getElementById('copyBtn3')
+   .addEventListener('click', function(){
+       showToast();
+       count ++;
+       copyCounter.textContent = count;
+   })
+            //  copyBtn4 
+   document.getElementById('copyBtn4')
+   .addEventListener('click', function(){
+       showToast();
+       count ++;
+       copyCounter.textContent = count;
+   })
+            //  copyBtn5 
+   document.getElementById('copyBtn5')
+   .addEventListener('click', function(){
+       showToast();
+       count ++;
+       copyCounter.textContent = count;
+   })
+            //  copyBtn6 
+   document.getElementById('copyBtn6')
+   .addEventListener('click', function(){
+       showToast();
+       count ++;
+       copyCounter.textContent = count;
+   })
+            //  copyBtn7
+   document.getElementById('copyBtn7')
+   .addEventListener('click', function(){
+       showToast();
+       count ++;
+       copyCounter.textContent = count;
+   })
+            //  copyBtn8
+   document.getElementById('copyBtn8')
+   .addEventListener('click', function(){
+       showToast();
+       count ++;
+       copyCounter.textContent = count;
+   })
+            //  copyBtn9
+   document.getElementById('copyBtn9')
+   .addEventListener('click', function(){
+       showToast();
+       count ++;
+       copyCounter.textContent = count;
+   })
+            
